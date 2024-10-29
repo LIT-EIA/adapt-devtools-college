b420d630505fc33c5469120a909d04a3b7bbd1b0
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2024-10-29 16:31:31 -0400

    updated to work with new i18n

--- js/translation.js
+++ js/translation.js
@@ -1,20 +1,10 @@
 define(function(require) {
 
-    var en = require('../assets/lang/en');
-    var fr = require('../assets/lang/fr');
+    var en = require('./lang/en');
+    var fr = require('./lang/fr');
 
     function initializeI18N(){
         $.i18n().load({ 'en' : en, 'fr' : fr });
-        Handlebars.registerHelper('t', function(key, $1) {
-            var lang = localStorage.getItem("lang") || 'en';
-            $.i18n().locale = lang;
-            return $.i18n(key, $1);
-        });
-        Handlebars.registerHelper('tp', function(string, key) {
-            var lang = localStorage.getItem("lang") || 'en';
-            $.i18n().locale = lang;
-            return $.i18n(string+key);
-        });
     };
 
     return initializeI18N;
